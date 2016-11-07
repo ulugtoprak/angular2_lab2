33a009d21e40dd2ac550024f31bbb194ba0baa94
Enda Lee <enda.lee@ittdublin.ie>
2016-11-07 11:39:46 +0000

    Added Show message from input without click function

---
+++ src/app/click-me/click-me.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-me',
+  templateUrl: './click-me.component.html',
+  styleUrls: ['./click-me.component.css']
+})
+export class ClickMeComponent implements OnInit {
+  clickMessage: string = "";
+
+  constructor() { }
+onClickMe(){
+  this.clickMessage = "you click the button!";
+}
+  ngOnInit() {
+  }
+
+}
